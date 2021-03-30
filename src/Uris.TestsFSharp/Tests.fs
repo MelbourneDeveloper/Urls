0d3d3d711f5c1171fd13d528e2fd5c8580b31ade
Christian Findlay <a@a.com>
2021-03-30 20:10:57 +1100

    Add F# tests

---
+++ src/Uris.TestsFSharp/Tests.fs
@@ -0,0 +1,11 @@
+namespace Uris.TestsFSharp
+
+open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+[<TestClass>]
+type TestClass () =
+
+    [<TestMethod>]
+    member this.TestMethodPassing () =
+        Assert.IsTrue(true);
--- src/Uris/RelativeUri.cs
+++ src/Uris/RelativeUri.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
-using System.Net;
 
 namespace Uris
 {
