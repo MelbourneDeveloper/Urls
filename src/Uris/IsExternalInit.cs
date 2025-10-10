e76f0de486d1cb392085a9a7b91816dbab94f59b
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2025-10-10 20:38:53 +1100

    Config

--- src/Uris/IsExternalInit.cs
+++ src/Uris/IsExternalInit.cs
@@ -1,6 +1,10 @@
 ﻿#pragma warning disable IDE0130 // Namespace does not match folder structure
+#pragma warning disable CA1515 // Consider making public types internal
+
 namespace System.Runtime.CompilerServices
-#pragma warning restore IDE0130 // Namespace does not match folder structure
 {
     public class IsExternalInit { }
 }
+
+#pragma warning restore IDE0130 // Namespace does not match folder structure
+#pragma warning restore CA1515 // Consider making public types internal
